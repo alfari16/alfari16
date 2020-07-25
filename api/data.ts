2b88a1d4dcb73628e41054f98f50c9ea06c6e361
alfari16 <alif.anshory@yahoo.com>
2020-07-25 19:46:24 +0700

    delete cached file

--- api/data.ts
+++
@@ -1,8 +0,0 @@
-import { NowRequest, NowResponse } from '@vercel/node';
-import { ghRepo } from '../util/github';
-
-export default async (req: NowRequest, res: NowResponse) => {
-  const data = await ghRepo.contentsAsync('data/tictactoe.json');
-  const parsed = Buffer.from(data[0].content, 'base64').toString('utf-8');
-  return res.json({ data, parsed, origin: req.headers.origin });
-};
