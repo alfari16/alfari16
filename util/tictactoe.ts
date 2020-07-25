aa483d7209f5e8a7178e56232e3119ff67c179ba
alfari16 <alif.anshory@yahoo.com>
2020-07-25 20:17:57 +0700

    fixed abstraction code

--- util/tictactoe.ts
+++ util/tictactoe.ts
@@ -13,4 +13,24 @@ export const currentTurn = async (tictactoeData = null): Promise<string> => {
   return xCount <= oCount ? 'X' : 'O';
 };
 
-export const isComplete = async (tictactoeData) => {};
+export const hasWinner = async (tictactoeData = null) => {
+  tictactoeData = tictactoeData || (await dataRaw()).data;
+  console.log(tictactoeData);
+
+  const x = tictactoeData.filter((el) => el.value === 'X').map((el) => el.code);
+  const o = tictactoeData.filter((el) => el.value === 'O').map((el) => el.code);
+
+  const xWord = x.join('').replace(/\d/g, '');
+  const xDigit = x.join('').replace(/\D/g, '');
+  console.log(xWord, xDigit);
+  if (/A{3}|B{3}|C{3}/gm.test(xWord) || /1{3}|2{3}|3{3}/gm.test(xDigit))
+    return 'X';
+
+  const oWord = o.join('').replace(/\d/g, '');
+  const oDigit = o.join('').replace(/\D/g, '');
+  console.log(oWord, oDigit);
+
+  if (/A{3}|B{3}|C{3}/gm.test(oWord) || /1{3}|2{3}|3{3}/gm.test(oDigit))
+    return 'O';
+  return false;
+};
